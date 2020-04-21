02ee76e1ca4277969a80618678cb9b6c93766b39
Hendrik Schellenberger <hkonitzer@googlemail.com>
2020-04-21 21:13:36 +0200

    WIP: html rendering

--- routes/forms.js
+++ routes/forms.js
@@ -23,10 +23,14 @@ if (schemaFiles.length) {
   process.exit(1);
 }
 
-const buildAndSendResponse = function(req, res, html) {
-  let _html = `<html><head></head><body><form method="post" action="${req.originalUrl}">${html}<input type="hidden" name="_csrf" value="${req.csrfToken()}"></form></body></html>`;
+const buildAndSendResponse = function(req, res, html, message = "", jsonPaylod = null, responseCode = 200) {
+  let _html = `<html><head></head><body><div class="formMessage">${message}</div><form method="post" action="${req.originalUrl}">${html}<input type="hidden" name="_csrf" value="${req.csrfToken()}"></form>`; //
+  if (jsonPaylod !== null) {
+    _html+=`<script>let formJSONPayload = ${JSON.stringify(jsonPaylod)}</script>`;
+  }
+  _html+='</body></html>';
   res.set('Content-Type', 'text/html');
-  return res.status(200).send(_html);
+  return res.status(responseCode).send(_html);
 };
 
 for (let s in schemaFiles) {
@@ -45,28 +49,30 @@ for (let s in schemaFiles) {
   html += `<!--end ${endpoint} -->`;
 
   // GET
-  router.get(endpoint, csrfProtection, (req, res) => buildAndSendResponse(req, res, html));
+  router.get(endpoint, csrfProtection, (req, res) => {
+    return buildAndSendResponse(req, res, html)
+  });
   // POST
   router.post(endpoint, checkSchema(schema.definition), csrfProtection, (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      // @TODO: SEND HTML!
+      return buildAndSendResponse(req, res, html, "FEHLER!", { errors: errors.array() }, 400);
+
+      /*
+      delete req.body._csrf;
       return res.status(400).send({
         data: req.body,
         errors: errors.array(),
         errorMap: errors.mapped()
       });
+      */
     }
 
     const data = matchedData(req);
     console.log('Sanitized: ', data)
     // @TODO: send sanitized data in an email or persist in a db
 
-    return res.status(201).send({
-      data: data,
-      errors: null,
-      errorMap: null
-    });
+    return res.status(201).send(`<html><head></head><body><div>Daten übertragen!</div></body></html>`);
   });
 
 }
